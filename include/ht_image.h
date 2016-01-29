1dd58b7bfa6f8d33bc25fc338c4dca2672853dcf
MattGuerrette <direct3Dtutorials@gmail.com>
2016-01-28 23:53:19 -0500

    migrated HatchitImage source to resource lib

---
+++ include/ht_image.h
@@ -0,0 +1,45 @@
+/**
+**    Hatchit Engine
+**    Copyright(c) 2015 Third-Degree
+**
+**    GNU Lesser General Public License
+**    This file may be used under the terms of the GNU Lesser
+**    General Public License version 3 as published by the Free
+**    Software Foundation and appearing in the file LICENSE.LGPLv3 included
+**    in the packaging of this file. Please review the following information
+**    to ensure the GNU Lesser General Public License requirements
+**    will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**/
+
+/**
+    Based on Simple OpenGL Image Library
+        ** Original author Jonathan Dummer
+
+    Uses Sean Barret's Tool Box image loader as a base:
+    http://www.nothings.org/
+
+  
+    Image Formats:
+    - BMP		load & save
+    - TGA		load & save
+    - DDS		load & save
+    - PNG		load & save
+    - JPG		load
+    - PSD		load
+    - HDR		load
+    - PIC		load
+
+    Thanks to:
+    * Sean Barret - for stb_image
+**/
+
+#pragma once
+
+namespace Hatchit {
+
+    namespace Image {
+
+
+    }
+}
