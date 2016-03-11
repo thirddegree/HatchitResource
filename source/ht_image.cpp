03f984390e705537e38bcec7724aa5095bc2d571
MattGuerrette <direct3Dtutorials@gmail.com>
2016-03-11 15:51:50 -0500

    Changed bitmap to image

--- source/ht_image.cpp
+++ source/ht_image.cpp
@@ -12,12 +12,94 @@
 **
 **/
 
+#include <ht_image.h>
+#include <stb_image.h>
+
 namespace Hatchit {
 
-    namespace Image {
+    namespace Resource {
 
+        Image::Image()
+        {
+            m_width = 0;
+            m_height = 0;
+            m_channels = 0;
+            m_data = nullptr;
+        }
 
+        Image::~Image()
+        {
+            stbi_image_free(m_data);
+        }
 
+        const int32_t Image::GetWidth() const
+        {
+            return m_width;
+        }
+
+        const int32_t Image::GetHeight() const
+        {
+            return m_height;
+        }
+
+        const int32_t Image::GetChannels() const
+        {
+            return m_channels;
+        }
+
+        const int32_t Image::GetBPP() const
+        {
+            return m_bpp;
+        }
+
+        const BYTE* Image::GetData() const
+        {
+            return m_data;
+        }
+
+        ////////////////////////////////////////////////
+        // Bitmap static functions
+        ////////////////////////////////////////////////
+
+        Image* Image::Load(Core::File* file, Channels channels)
+        {
+            Image* bitmap = new Image;
+
+            int req_channels = 0;
+            switch (channels)
+            {
+            case Channels::AUTO:
+                req_channels = 0;
+                break;
+            case Channels::L:
+                req_channels = 1;
+                bitmap->m_bpp = 32;
+                break;
+            case Channels::LA:
+                req_channels = 2;
+                bitmap->m_bpp = 32;
+                break;
+            case Channels::RGB:
+                req_channels = 3;
+                bitmap->m_bpp = 32;
+                break;
+            case Channels::RGBA:
+                req_channels = 4;
+                bitmap->m_bpp = 32;
+                break;
+
+            default:
+                break;
+            }
+
+            bitmap->m_data = stbi_load_from_file(file->Handle(),
+                &bitmap->m_width, &bitmap->m_height, &bitmap->m_channels,
+                req_channels);
+            if (channels == Channels::AUTO)
+                bitmap->m_bpp = bitmap->m_channels * 8;
+
+            return bitmap;
+        }
     }
 
 }
