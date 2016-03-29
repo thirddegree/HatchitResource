e514b32b9e2b76f4759dd1c0d6522b0077e7df83
MattGuerrette <direct3Dtutorials@gmail.com>
2016-03-29 19:59:47 -0400

    created general resource manager for data.

---
+++ source/ht_resourcemanager.cpp
@@ -0,0 +1,44 @@
+/**
+**    Hatchit Engine
+**    Copyright(c) 2015-2016 Third-Degree
+**
+**    GNU Lesser General Public License
+**    This file may be used under the terms of the GNU Lesser
+**    General Public License version 3 as published by the Free
+**    Software Foundation and appearing in the file LICENSE.LGPLv3 included
+**    in the packaging of this file. Please review the following information
+**    to ensure the GNU Lesser General Public License requirements
+**    will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**/
+
+#include <ht_resourcemanager.h>
+
+namespace Hatchit
+{
+	namespace Resource
+	{
+		void ResourceManager::Initialize()
+		{
+
+		}
+
+		void ResourceManager::DeInitialize()
+		{
+
+		}
+
+		void ResourceManager::ReleaseRawPointer(const std::string& name)
+		{
+			ResourceManager& _instance = ResourceManager::instance();
+
+			std::map<std::string, void*>::iterator it = _instance.m_resources.find(name);
+			if (it != _instance.m_resources.end())
+			{
+				delete _instance.m_resources[name];
+
+				_instance.m_resources.erase(it);
+			}
+		}
+	}
+}
