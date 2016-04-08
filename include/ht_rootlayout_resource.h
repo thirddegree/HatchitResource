5ea1c704bc450410fb6d98f54eab5ffadc042c2c
Honeybunch <amt3824@g.rit.edu>
2016-04-08 04:29:16 -0400

    Spaces

--- include/ht_rootlayout_resource.h
+++ include/ht_rootlayout_resource.h
@@ -18,117 +18,117 @@
 
 namespace Hatchit
 {
-	namespace Resource
-	{
-		class HT_API RootLayout : public FileResource<RootLayout>
-		{
-		public:
+    namespace Resource
+    {
+        class HT_API RootLayout : public FileResource<RootLayout>
+        {
+        public:
 
-			enum RootDescriptorFlags
-			{
-				DESCRIPTOR_FLAG_NONE = 0,
-				DESCRIPTOR_ALLOW_INPUT_ASSEMLBER_INPUT_LAYOUT,
-				DESCRIPTOR_DENY_VERTEX_SHADER_ROOT_ACCESS,
-				DESCRIPTOR_DENY_TESSELATION_CONTROL_SHADER_ROOT_ACCESS,
-				DESCRIPTOR_DENY_TESSELATION_EVAL_SHADER_ROOT_ACCESS,
-				DESCRIPTOR_DENY_GEOMETRY_SHADER_ROOT_ACCESS,
-				DESCRIPTOR_DENY_FRAGMENT_SHADER_ROOT_ACCESS0,
-				DESCRIPTOR_ALLOW_STREAM_OUTPUT
-			};
+            enum RootDescriptorFlags
+            {
+                DESCRIPTOR_FLAG_NONE = 0,
+                DESCRIPTOR_ALLOW_INPUT_ASSEMLBER_INPUT_LAYOUT,
+                DESCRIPTOR_DENY_VERTEX_SHADER_ROOT_ACCESS,
+                DESCRIPTOR_DENY_TESSELATION_CONTROL_SHADER_ROOT_ACCESS,
+                DESCRIPTOR_DENY_TESSELATION_EVAL_SHADER_ROOT_ACCESS,
+                DESCRIPTOR_DENY_GEOMETRY_SHADER_ROOT_ACCESS,
+                DESCRIPTOR_DENY_FRAGMENT_SHADER_ROOT_ACCESS0,
+                DESCRIPTOR_ALLOW_STREAM_OUTPUT
+            };
 
-			struct Range
-			{
-				enum class Type
-				{
-					UKNOWN,
-					CONSTANT_BUFFER,
-					SHADER_RESOURCE,
-					UNORDERED_ACCESS,
-					SAMPLER
-				};
+            struct Range
+            {
+                enum class Type
+                {
+                    UKNOWN,
+                    CONSTANT_BUFFER,
+                    SHADER_RESOURCE,
+                    UNORDERED_ACCESS,
+                    SAMPLER
+                };
 
-				Type		type;
-				uint32_t	numDescriptors;
-				uint32_t	baseRegister;
-				uint32_t	registerSpace;
-			};
+                Type		type;
+                uint32_t	numDescriptors;
+                uint32_t	baseRegister;
+                uint32_t	registerSpace;
+            };
 
-			struct DescriptorTable
-			{
-				uint32_t			rangeCount;
-				std::vector<Range>	ranges;
-			};
+            struct DescriptorTable
+            {
+                uint32_t			rangeCount;
+                std::vector<Range>	ranges;
+            };
 
-			struct Constant
-			{
-				uint32_t shaderRegister;
-				uint32_t registerSpace;
-				uint32_t valueCount;
-			};
+            struct Constant
+            {
+                uint32_t shaderRegister;
+                uint32_t registerSpace;
+                uint32_t valueCount;
+            };
 
-			struct Descriptor
-			{
-				uint32_t shaderRegister;
-				uint32_t registerSpace;
-			};
+            struct Descriptor
+            {
+                uint32_t shaderRegister;
+                uint32_t registerSpace;
+            };
 
-			struct Parameter
-			{
-				struct Data
-				{
-					DescriptorTable table;
-					Constant		constant;
-					Descriptor	    descriptor;
-				};
+            struct Parameter
+            {
+                struct Data
+                {
+                    DescriptorTable table;
+                    Constant		constant;
+                    Descriptor	    descriptor;
+                };
 
-				enum class Type
-				{
-					UNKNOWN,
-					TABLE,
-					CONSTANT,
-					CONSTANT_BUFFER,
-					SHADER_RESOURCE,
-					UNORDERED_ACCESS
-				};
+                enum class Type
+                {
+                    UNKNOWN,
+                    TABLE,
+                    CONSTANT,
+                    CONSTANT_BUFFER,
+                    SHADER_RESOURCE,
+                    UNORDERED_ACCESS
+                };
 
-				enum class Visibility
-				{
-					UNKNOWN,
-					ALL,
-					VERTEX,
-					TESS_CONTROL,
-					TESS_EVAL,
-					GEOMETRY,
-					FRAGMENT
-				};
+                enum class Visibility
+                {
+                    UNKNOWN,
+                    ALL,
+                    VERTEX,
+                    TESS_CONTROL,
+                    TESS_EVAL,
+                    GEOMETRY,
+                    FRAGMENT
+                };
 
-				Type			type;
-				Visibility		visibility;
-				Data			data;
-			};
+                Type			type;
+                Visibility		visibility;
+                Data			data;
+            };
 
-			RootLayout(std::string ID, const std::string& fileName);
+            RootLayout(std::string ID, const std::string& fileName);
 
-			virtual ~RootLayout() = default;
-
-
-			uint32_t						GetParameterCount() const;
-			RootDescriptorFlags				GetDescriptorFlags() const;
-			const std::vector<Parameter>&	GetParameters() const;
-
-			virtual bool VInitFromFile(const std::string& fileName);
-
-		private:
-			uint32_t				m_parameterCount;
-			std::vector<Parameter>	m_parameters;
-			RootDescriptorFlags		m_flags;
+            virtual ~RootLayout() = default;
 
 
-			Parameter::Visibility	ParameterVisibilityFromString(std::string s);
-			Parameter::Type			ParameterTypeFromString(std::string s);
-			Range::Type				RangeTypeFromString(std::string s);
-		};
+            uint32_t                        GetParameterCount() const;
+            RootDescriptorFlags             GetDescriptorFlags() const;
+            const std::vector<Parameter>&   GetParameters() const;
 
-		using RootLayoutHandle = Core::Handle<const RootLayout>;
-	}
+            virtual bool VInitFromFile(const std::string& fileName);
+
+        private:
+            uint32_t                m_parameterCount;
+            std::vector<Parameter>  m_parameters;
+            RootDescriptorFlags     m_flags;
+
+
+            Parameter::Visibility   ParameterVisibilityFromString(std::string s);
+            Parameter::Type			ParameterTypeFromString(std::string s);
+            Range::Type				RangeTypeFromString(std::string s);
+        };
+
+        using RootLayoutHandle = Core::Handle<const RootLayout>;
+    }
 }
